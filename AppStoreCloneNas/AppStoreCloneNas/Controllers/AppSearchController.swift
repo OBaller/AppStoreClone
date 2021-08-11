24296df324467d0d9768be9afdeaec1561e72d2d
OBaller <naseemoyebola@gmail.com>
2021-08-11 22:38:47 +0100

    cells and UIStackview layout for search viewController

---
+++ AppStoreCloneNas/AppStoreCloneNas/Controllers/AppSearchController.swift
@@ -0,0 +1,43 @@
+//
+//  AppSearchController.swift
+//  AppStoreCloneNas
+//
+//  Created by Decagon on 11/08/2021.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class AppSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    fileprivate let cellId = "id1234"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .white
+        collectionView.register(SearchResultCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        return  cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: 250)
+    }
+    
+    init() {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
