12d6d2a2d8b24c8b1e39be9b6ccc5f440c34bc8f
OBaller <naseemoyebola@gmail.com>
2021-08-11 16:01:49 +0100

    tab bar image

--- AppStoreCloneNas/AppStoreCloneNas/BaseTabBarController.swift
+++ AppStoreCloneNas/AppStoreCloneNas/BaseTabBarController.swift
@@ -13,13 +13,23 @@ class BaseTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let todayController = UIViewController()
+        todayController.view.backgroundColor = .white
+        todayController.navigationItem.title = "Today"
+        
+        let todayNavController = UINavigationController(rootViewController: todayController)
+        todayNavController.tabBarItem.title = "Today"
+        todayNavController.tabBarItem.image = UIImage(systemName: "square.and.arrow.down")
+        todayNavController.navigationBar.prefersLargeTitles = true
+        
+        
         let redViewController = UIViewController()
         redViewController.view.backgroundColor = .white
         redViewController.navigationItem.title = "Apps"
         
         let redNavController = UINavigationController(rootViewController: redViewController)
         redNavController.tabBarItem.title = "Apps"
-        //redNavController.tabBarItem.image = UIImage(systemName: "Home")
+        redNavController.tabBarItem.image = UIImage(systemName: "square.and.pencil")
         redNavController.navigationBar.prefersLargeTitles = true
         
         
@@ -29,12 +39,12 @@ class BaseTabBarController: UITabBarController {
         
         let blueNavController = UINavigationController(rootViewController: blueViewController)
         blueNavController.tabBarItem.title = "Search"
-        //blueNavController.tabBarItem.image = #imageLiteral(resourceName: "magnifying-glass-computer-file-png-favpng-jHQnTzv92DVQiUPttsZ1G8VcK_t")
+        blueNavController.tabBarItem.image = UIImage(systemName: "tray")
         blueNavController.navigationBar.prefersLargeTitles = true
         
         
         viewControllers = [
-            redNavController, blueNavController
+           todayNavController, redNavController, blueNavController
         ]
     }
 }
