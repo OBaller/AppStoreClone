e09a55d8b9b3758c6150c11134b9651fd83f3287
OBaller <naseemoyebola@gmail.com>
2021-08-14 18:41:25 +0100

    Feat: apps collection view cell

---
+++ AppStoreCloneNas/AppStoreCloneNas/Apps/AppsController.swift
@@ -0,0 +1,43 @@
+//
+//  AppsController.swift
+//  AppStoreCloneNas
+//
+//  Created by Decagon on 13/08/2021.
+//
+
+import UIKit
+
+class AppsController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+   fileprivate let celId = "id"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .yellow
+        
+        collectionView.register(AppsGroupCell.self, forCellWithReuseIdentifier: celId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: celId, for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: 250)
+    }
+    
+    init() {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
+
+}
---
+++ AppStoreCloneNas/AppStoreCloneNas/Views/Apps/AppsGroupCell.swift
@@ -0,0 +1,21 @@
+//
+//  AppsGroupCell.swift
+//  AppStoreCloneNas
+//
+//  Created by Decagon on 14/08/2021.
+//
+
+import UIKit
+
+class AppsGroupCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .purple
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
